314b7777276b9981fccefc03922d02a22d1327a0
virtouso <s_moeen_kamali@yahoo.com>
2023-10-15 04:36:54 +0330

    first api

---
+++ Microservice1/src/main.rs
@@ -0,0 +1,17 @@
+#[macro_use] extern crate rocket;
+
+
+// fn main() {
+//     println!("Hello, world!");
+// }
+
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
+}
